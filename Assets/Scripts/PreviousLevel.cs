468f531a47a1b9ff2533106e680ceaf2863abae9
GRANT HOECHST <granthoechst@college.harvard.edu>
2016-10-28 00:43:43 -0400

    added unity project and builds

---
+++ Assets/Scripts/PreviousLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreviousLevel : MonoBehaviour {
+
+	// respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the PreviousLevel function in the game manager
+			GameManager.gm.PreviousLevel();
+		}
+	}
+}
